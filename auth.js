915026216a53be23be7cb28da937b3ea8ba7e698
nguyen2007 <84222655+nguyen2007@users.noreply.github.com>
2026-03-06 14:51:07 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -10,21 +10,27 @@ onAuthStateChanged,
 signOut
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyC5QcVkCTr1KhwqBvRARI3yEVMTocyCfsc",
+  authDomain: "trackpay-app-fa1ff.firebaseapp.com",
+  projectId: "trackpay-app-fa1ff",
+  storageBucket: "trackpay-app-fa1ff.firebasestorage.app",
+  messagingSenderId: "563980092933",
+  appId: "1:563980092933:web:5ba15609ed78dc5b1afe6c",
+  measurementId: "G-9Q76DWEJR3"
 };
 
-
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
@@ -105,6 +111,7 @@ window.location.href = "login.html";
 }
 
 });
+
 
 
 
