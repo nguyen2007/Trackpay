c7434709151de3969043365c82e4a83aefcd33b6
nguyen2007 <84222655+nguyen2007@users.noreply.github.com>
2026-03-06 14:36:48 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -38,20 +38,13 @@ createUserWithEmailAndPassword(auth, email, password)
 
 alert("Account created!");
 window.location.href = "index.html";
+})
 
 .catch((error)=>{
 
-alert(error.message);
-
-});
+alert(error.message));
 
 }
-  
-})
-.catch(error => alert(error.message));
-
-}
-
 window.googleLogin = function(){
 
 signInWithPopup(auth, provider)
@@ -65,6 +58,12 @@ console.log("Google User:", user);
 window.location.href = "index.html";
 
 })
+
+.catch((error)=>{
+
+alert(error.message));
+
+}
 
 window.login = function(){
 
@@ -106,6 +105,7 @@ window.location.href = "login.html";
 }
 
 });
+
 
 
 
