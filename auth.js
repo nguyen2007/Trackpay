542df166cbcbdc13cb178a9e07c4a1c011d264d0
nguyen2007 <84222655+nguyen2007@users.noreply.github.com>
2026-03-06 14:08:12 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,21 +1,78 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getAuth, 
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+onAuthStateChanged,
+signOut
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC5QcVkCTr1KhwqBvRARI3yEVMTocyCfsc",
-  authDomain: "trackpay-app-fa1ff.firebaseapp.com",
-  projectId: "trackpay-app-fa1ff",
-  storageBucket: "trackpay-app-fa1ff.firebasestorage.app",
-  messagingSenderId: "563980092933",
-  appId: "1:563980092933:web:5ba15609ed78dc5b1afe6c",
-  measurementId: "G-9Q76DWEJR3"
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+window.signup = function(){
+
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+
+createUserWithEmailAndPassword(auth, email, password)
+.then(() => {
+
+alert("Account created!");
+window.location.href = "index.html";
+
+})
+.catch(error => alert(error.message));
+
+}
+
+window.login = function(){
+
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+
+signInWithEmailAndPassword(auth, email, password)
+.then(() => {
+
+window.location.href = "index.html";
+
+})
+.catch(error => alert(error.message));
+
+}
+
+window.logout = function(){
+
+signOut(auth).then(() => {
+
+window.location.href = "login.html";
+
+})
+
+}
+
+onAuthStateChanged(auth, user => {
+
+if(user){
+
+console.log("Logged in:", user.email);
+
+}else{
+
+if(window.location.pathname.includes("index.html")){
+window.location.href = "login.html";
+}
+
+}
+
+});
